a4557eae8008c94a3271ee4e36b32d0753cf6edd
qiphon <13223394160@sina.cn>
2019-10-28 23:43:14 +0800

    add python

---
+++ python/demo/basic.py
@@ -0,0 +1,55 @@
+# 我是单行注释
+
+''' 
+我是多行注释
+
+'''
+# print('hello world');
+
+# 打印关键字
+# import keyword;
+
+# print(keyword.kwlist)
+
+# 定义变量名的方式
+# 变量名 = 变量值
+# name = "qiphon"
+# height= 175.00
+# weight= 75
+# # # 打印信息
+# # print('我是', name,"体重：", weight)
+
+# # 打印数据类型
+# print('height:', type(height), 'weight:', type(weight))
+# # height: <class 'float'> weight: <class 'int'>
+
+# float
+# pi = 3.14159
+# r = 3
+# area = pi * r ** 2
+# print('pi:', pi, type(pi))
+# print('r:', r, type(r))
+# print('area:', area, type(area))
+# # pi: 3.14159 <class 'float'>
+# # r: 3 <class 'int'>
+# # area: 28.27431 <class 'float'>
+
+
+# 数字类型转换
+
+a1 = int('123')
+a2 = int(123.4)
+# a3 = int('12x')  报错
+f1 = float('123')
+f2 = float('123.12')
+# f1 = float('123.12x')  报错
+s1 = str(123)
+s2 = str(123.23)
+print('a1->type:',type(a1) , 'value:' , a1)
+print('a2->type:',type(a2) , 'value:' , a2)
+
+print('f1->type:',type(f1) , 'value:' , f1)
+print('f2->type:',type(f2) , 'value:' , f2)
+
+print('s1->type:',type(s1) , 'value:' , s1)
+print('s2->type:',type(s2) , 'value:' , s2)
---
+++ python/readme.md
@@ -0,0 +1,5 @@
+# python 学习笔记
+
+1. 认识python、安装学习相关软件
+
+2. python 语法
